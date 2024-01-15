25ff41d34bebacec45513e43df7ca7a42409951e
Taha <yhasinmuhammad123@gmail.com>
2024-01-14 20:42:52 -0700

    added more

--- s3.tf
+++ s3.tf
@@ -7,8 +7,17 @@ resource "aws_s3_bucket" "bucket1" {
   }
 }
 
-resource "aws_s3_bucket_lifecycle" "lifecycle" {
-  bucket = aws_s3_bucket.bucket1.bucket
+resource "aws_s3_bucket_public_access_block" "bucket_acces" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_lifecycle" "lifecycle_rule" {
+  bucket = aws_s3_bucket.bucket1.id
 
   rule {
     id      = "move-to-glacier"
@@ -24,6 +33,65 @@ resource "aws_s3_bucket_lifecycle" "lifecycle" {
       days = 1825 
     }
   }
+}
+
+
+resource "aws_iam_role" "s3_ec2_role" {
+  name = "s3_ec2_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "s3_ec2_role"
+  }
+}
+
+resource "aws_iam_policy" "s3_policy" {
+  name        = "s3_policy"
+  path        = "/"
+  description = ""
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:DeleteObject"
+        ],
+        "Resource" : [
+          "arn:aws:s3:::*/*",
+          "arn:aws:s3:::my-bucket-name"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_policy_attach" {
+  role       = aws_iam_role.s3_ec2_role.name
+  policy_arn = aws_iam_policy.s3_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_policy_attach" {
+  role       = aws_iam_role.s3_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
 
